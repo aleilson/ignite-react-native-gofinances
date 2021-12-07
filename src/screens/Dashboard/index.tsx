619210a63b88907b029fbb2194e60db5b9fbe944
Aleilson Gomes de Cerqueira <aleilson.cerqueira@compasso.com.br>
2021-12-07 12:22:07 -0300

    Add user profile and balance card

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { HighlightCard } from '../../Components/HighlightCard';
+import { 
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighlightCards
+} from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://github.com/aleilson.png'}} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Aleilson</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
+
+            <HighlightCards>
+                <HighlightCard />
+                <HighlightCard />
+                <HighlightCard />
+                <HighlightCard />
+            </HighlightCards>
         </Container>
     )
 }
