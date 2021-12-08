c8f72360eb598ae9a5117c6fd124904899010dbf
Aleilson Gomes de Cerqueira <aleilson.cerqueira@compasso.com.br>
2021-12-08 12:57:36 -0300

    Add react navigation and fix buttons

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -14,7 +14,8 @@ import {
     HighlightCards,
     Transactions,
     Title,
-    TransactionList
+    TransactionList,
+    LogoutButton
 } from './styles';
 
 export interface DataListProps extends TransactionCardProps {
@@ -69,7 +70,9 @@ export function Dashboard() {
                             <UserName>Aleilson</UserName>
                         </User>
                     </UserInfo>
-                    <Icon name="power" />
+                    <LogoutButton onPress={() => {}}>
+                        <Icon name="power" />
+                    </LogoutButton>
                 </UserWrapper>
             </Header>
 
